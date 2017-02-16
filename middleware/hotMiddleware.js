fe61330dafb1bb46e3b25e2f096cc306adc85e56
noinkling <noinkling@users.noreply.github.com>
2017-02-16 18:10:18 +1300

    Don't prematurely set hot middleware body/headers, and fix status

--- middleware/hotMiddleware.js
+++ middleware/hotMiddleware.js
@@ -5,11 +5,12 @@ export default (compiler, opts) => {
   const expressMiddleware = hotMiddleware(compiler, opts)
   return async (ctx, next) => {
     let stream = new PassThrough()
-    ctx.body = stream
+
     await expressMiddleware(ctx.req, {
       write: stream.write.bind(stream),
-      writeHead: (state, headers) => {
-        ctx.state = state
+      writeHead: (status, headers) => {
+        ctx.body = stream
+        ctx.status = status
         ctx.set(headers)
       }
     }, next)
