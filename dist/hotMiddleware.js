826ea86a49a7ade3f579f9d3b54d27a73634dd2a
noinkling <noinkling@users.noreply.github.com>
2017-04-02 14:02:35 +1200

    Build for Node 7.6+

--- dist/hotMiddleware.js
+++ dist/hotMiddleware.js
@@ -12,38 +12,18 @@ var _stream = require('stream');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+exports.default = (compiler, opts) => {
+  const expressMiddleware = (0, _webpackHotMiddleware2.default)(compiler, opts);
+  return async (ctx, next) => {
+    let stream = new _stream.PassThrough();
 
-exports.default = function (compiler, opts) {
-  var expressMiddleware = (0, _webpackHotMiddleware2.default)(compiler, opts);
-  return function () {
-    var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
-      var stream;
-      return regeneratorRuntime.wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              stream = new _stream.PassThrough();
-              _context.next = 3;
-              return expressMiddleware(ctx.req, {
-                write: stream.write.bind(stream),
-                writeHead: function writeHead(status, headers) {
-                  ctx.body = stream;
-                  ctx.status = status;
-                  ctx.set(headers);
-                }
-              }, next);
-
-            case 3:
-            case 'end':
-              return _context.stop();
-          }
-        }
-      }, _callee, undefined);
-    }));
-
-    return function (_x, _x2) {
-      return _ref.apply(this, arguments);
-    };
-  }();
+    await expressMiddleware(ctx.req, {
+      write: stream.write.bind(stream),
+      writeHead: (status, headers) => {
+        ctx.body = stream;
+        ctx.status = status;
+        ctx.set(headers);
+      }
+    }, next);
+  };
 };
